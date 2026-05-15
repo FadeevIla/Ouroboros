73c4af457477cc15eb8e85bcb190278b5a917907
Ryan <ilaadth1@gmail.com>
2026-05-15 22:57:44 +0500

    TG-уведомления теперь содержат конкретное описание изменений

--- self_fixing_bot.py
+++ self_fixing_bot.py
@@ -224,6 +224,8 @@ class DarwinOrchestrator:
                         # Генерируем описание исправления
                         description = self._describe_changes(code, validated_code)
                         self._save_update_description(description, commit_sha)
+                        self.notifier.send(f"🐛 Исправлено: {description}", "fix")  # для багов
+                        self.notifier.send(f"✨ Добавлено: {description}", "feature")  # для фич
 
                         code, sha = validated_code, commit_sha
                     else:
@@ -268,6 +270,7 @@ class DarwinOrchestrator:
                             # Генерируем описание новой фичи
                             description = self._describe_changes(code, validated_code)
                             self._save_update_description(description, commit_sha)
+                            self.notifier.send(f"✨ Добавлено: {description}", "feature")  # ← ДОБАВЬ ЭТУ СТРОКУ
 
                             code, sha = validated_code, commit_sha
                         else:
